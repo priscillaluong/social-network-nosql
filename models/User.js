5b2444239ce9a46872b61194d6dcc6127ed99db9
P Luong <p-luong@live.co.uk>
2022-11-03 14:49:42 +0000

    created User schema

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+const Thought = require('./Thought');
+
+// Schema to create Post model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true, 
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, 
+
+    },
+    thoughts: [Thought],
+    friends: {
+      type: String,
+      minLength: 15,
+      maxLength: 500,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `getTags` that gets the amount of tags associated with an application
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+// Initialize our Application model
+const User = model('user', userSchema);
+
+module.exports = User;
